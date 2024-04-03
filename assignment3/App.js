c6b31b4623de7a1f4705cbb9073876bbe9d9abd6
DBugghi <dibugghi@gmail.com>
2024-04-04 00:10:13 +0530

    uncompleted assignment 3

---
+++ assignment3/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import loggo from "./google.png";
+import usrr from "./user.png";
+
+const logo = (<img src={loggo} style={{width: 100, height: 100}}/>);
+const inp = (<input>Search here</input>);
+const usr = (<img src={usrr} style={{widht:100, height:100}}></img>);
+
+const Header = () => (
+    <div id="container">
+        XYZ
+    </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header/>);
