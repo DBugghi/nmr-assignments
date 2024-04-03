5b5e45ca317448b249b5dc5457872d80d6de0ba0
DBugghi <dibugghi@gmail.com>
2024-04-03 16:22:00 +0530

    ca-2

---
+++ assignment2/App.js
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom";
+
+const heading = (
+    <div className="title">
+        <h1>
+            <h2>
+                <h3>XYZ</h3>
+            </h2>
+        </h1>
+    </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
